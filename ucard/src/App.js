55d3b876d5fffd4ef872623b4fcbdd6155f49ce3
molopezCU <52024285+molopezCU@users.noreply.github.com>
2022-10-15 11:38:34 -0700

    uCard first compile

--- ucard/src/App.js
+++ ucard/src/App.js
@@ -1,3 +1,17 @@
+import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 
-import ./'App.css';
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="104267ed-fff8-4300-bbb9-2fca7e95e456"
+            userName="User1"
+            userSecret="secret"
+        />
+    );
+}
+
+export default App;
